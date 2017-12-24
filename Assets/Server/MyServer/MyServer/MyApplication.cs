28916a26be988b250c1499351fb4f5682ba447d5
marssuren <287815741@qq.com>
2017-12-24 20:18:21 +0800

    提交服务器端代码

---
+++ Assets/Server/MyServer/MyServer/MyApplication.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Photon.SocketServer;
+using Lite;
+
+namespace MyServer
+{
+    public class MyApplication : LiteApplication
+    {
+        protected override PeerBase CreatePeer(InitRequest _initRequest)//创建与客户端连接的Peer
+        {
+            //throw new NotImplementedException();
+            //return null;
+            MyPeer tMyPeer = new MyPeer(_initRequest);
+            return tMyPeer;
+        }
+
+        protected override void Setup()
+        {
+            //throw new NotImplementedException();
+
+        }
+
+        protected override void TearDown()
+        {
+            //throw new NotImplementedException();
+
+        }
+    }
+}
---
+++ Assets/Server/MyServer/MyServer/MyPeer.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Photon.SocketServer;
+using PhotonHostRuntimeInterfaces;
+using Lite.Operations;
+using Lite;
+
+namespace MyServer
+{
+	class MyPeer : PeerBase
+	{
+		public MyPeer(InitRequest initRequest)
+			: base(initRequest)
+		{
+		}
+
+		//public MyPeer(IRpcProtocol protocol, IPhotonPeer unmanagedPeer)
+		//    : base(protocol, unmanagedPeer)
+		//{
+		//}
+
+		protected override void OnOperationRequest(OperationRequest _operationRequest, SendParameters sendParameters)
+		{
+			switch(_operationRequest.OperationCode)
+			{
+				case (byte)OperationCode.Join:
+				Hashtable tGameProperties = (Hashtable)_operationRequest.Parameters[(byte)ParameterKey.GameProperties];
+				Hashtable tActorProperties =
+					(Hashtable)_operationRequest.Parameters[(byte)ParameterKey.ActorProperties];
+				string tUserName = tActorProperties[(byte)ChatEventKey.UserName].ToString();
+				OperationResponse tOperationResponse = new OperationResponse();
+				tOperationResponse.ReturnCode = 0;  //0表示返回成功
+				tOperationResponse.OperationCode = (byte)OperationCode.Join; //告诉客户端做出了哪种类型的响应
+				tOperationResponse.DebugMessage = "进入游戏房间成功！";
+				Dictionary<byte, object> tParaDic = new Dictionary<byte, object>();
+				tParaDic[(byte)ParameterKey.ActorNr] = 1;
+				tOperationResponse.Parameters = tParaDic;
+				SendOperationResponse(tOperationResponse, sendParameters);
+				break;
+				default:
+				break;
+			}
+		}
+
+		protected override void OnDisconnect(DisconnectReason reasonCode, string reasonDetail)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
+public enum ChatEventCode : byte            //仿照LiteEventCode自定义用于聊天功能的ChatEventCode
+{
+	Chat = 10,
+}
+public enum ChatEventKey : byte             //自定义用于聊天功能的ChatEventKey
+{
+	UserName = 11,
+	ChatMessage = 12,
+}
